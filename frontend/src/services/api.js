4459215c7f1c5b372cd2870af2b4e0a0540ce58a
ezaryan <aryan.pagar@somaiya.edu>
2026-06-19 01:38:40 +0530

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://inventorymanagement-production-86b6.up.railway.app/api",
+  
 });
 
 api.interceptors.request.use((config) => {
